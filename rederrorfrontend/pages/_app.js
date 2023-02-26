8ce14a85283e73f82df80972164785e3c9c7843b
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2023-02-26 17:30:48 +0100

    Update _app.js

--- rederrorfrontend/pages/_app.js
+++ rederrorfrontend/pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'.  I changed the CSS styling to SCSS which is a superset of CSS
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
